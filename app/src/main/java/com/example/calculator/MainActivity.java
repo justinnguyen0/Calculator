1802995692e270fc5dbe709d409abceda34d00bf
Justin Nguyen <justnguyen1@gmail.com>
2019-09-04 11:10:07 -0700

    Third commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView result;
+    EditText number1, number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
